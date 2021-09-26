942c029d50c7ccfdf29e763841580fc94e857b8a
houdini21 <marinerocruzfer@gmail.com>
2021-09-25 18:44:53 -0600

    :clown_face: se comenzo la maquetacion del login

--- application/views/frontend/sign-in.php
+++ application/views/frontend/sign-in.php
@@ -1,13 +1,53 @@
-<?php $this->load->view(template_frontpath("template/header")); ?>
-    <h2>crear cuenta</h2>
-    <form action="<?= base_url("/login/createUser") ?>" method="post">
-      <input type="text" name="email" id="email" placeholder="correo"/>
-      <br>
-      <br>
-      <input type="password" name="password" id="password" placeholder="contrasena"/>
-      <br>
-      <br>
-      <button type="submit">enviar</button>
-    </form>
-<?php $this->load->view(template_frontpath("template/footer")); ?>
+<?php $this->load->view(template_frontpath('template/header')); ?>
 
+<div
+  class="
+    h-screen
+    w-screen
+    bg-background-1 bg-center bg-cover
+    flex
+    justify-center
+    items-center
+  "
+>
+  <div class="bg-white opacity-75 h-96 w-80 p-8 rounded">
+    <h2 class="font-semibold text-3xl text-center">Crear Cuenta</h2>
+    <form action="<?= base_url("/login/createUser") ?>" method="post" class="h-5/6 flex flex-col justify-evenly py-8">
+      <input
+        class="
+          focus:border-green focus:ring-1 focus:ring-green focus:outline-none
+          w-full
+          text-base text-black
+          placeholder-gray-500
+          border border-gray-200
+          rounded-md
+          py-2
+          px-2
+          font-medium
+        "
+        type="text"
+        placeholder="Correo"
+        name="email"
+      />
+
+      <input
+        class="
+          focus:border-green focus:ring-1 focus:ring-green focus:outline-none
+          w-full
+          text-base text-black
+          placeholder-gray-500
+          border border-gray-200
+          rounded-md
+          py-2
+          px-2
+          font-medium
+          "
+        type="password"
+        placeholder="Contrasena"
+        name="password"
+      />
+    <button class="focus:bg-green-400 py-2 px-2 rounded bg-green text-white font-medium mt-4" type="submit">Siguiente &rarr;</button>
+    </form>
+  </div>
+</div>
+<?php $this->load->view(template_frontpath('template/footer')); ?>
